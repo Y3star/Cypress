417ad2b3e6d8492731560538275b68baa7cc1fa8
Y3star <117822700+Y3star@users.noreply.github.com>
2022-12-20 23:31:51 +0200

    up

--- cypress/e2e/tests/MainPage.cy.js
+++ cypress/e2e/tests/MainPage.cy.js
@@ -15,41 +15,53 @@ describe("Main page", () => {
     it("(Assert main-menu)", () => {
         //cant use hovar right now because my version 12 , and plagin real hovar work in 11v or lowest
         mainPage.clickElement(mainPage.products);
-        mainPage.getElement(mainPage.products).should("include.text", "Products");
+        mainPage
+            .getElement(mainPage.products)
+            .should("include.text", "Products");
         mainPage.clickElement(mainPage.solutions);
-        mainPage.solutions.should("include.text", "Solutions");
+        mainPage
+            .getElement(mainPage.solutions)
+            .should("include.text", "Solutions");
         mainPage.clickElement(mainPage.resources);
-        mainPage.resources.should("include.text", "Resources");
+        mainPage
+            .getElement(mainPage.resources)
+            .should("include.text", "Resources");
         mainPage.clickElement(mainPage.company);
-        mainPage.company.should("include.text", "Company");
+        mainPage.getElement(mainPage.company).should("include.text", "Company");
         mainPage.clickElement(mainPage.pricing);
-        mainPage.pricing.should("include.text", "Pricing");
+        mainPage.getElement(mainPage.pricing).should("include.text", "Pricing");
         cy.wait(300);
     });
     it("(Scroll and assert bottom title)", () => {
-        mainPage.productsBottomTitle
+        mainPage
+            .getElement(mainPage.productsBottomTitle)
             .should("be.visible")
             .should("include.text", "Products");
-        mainPage.resourcesBottomTitle
+        mainPage
+            .getElement(mainPage.resourcesBottomTitle)
             .should("be.visible")
             .should("include.text", "Resources");
-        mainPage.pricingBottomTitle
+        mainPage
+            .getElement(mainPage.pricingBottomTitle)
             .should("be.visible")
             .should("include.text", "Pricing");
-        mainPage.companyBottomTitle
+        mainPage
+            .getElement(mainPage.companyBottomTitle)
             .should("be.visible")
             .should("include.text", "Company");
-        mainPage.missControlBottomTitle
+        mainPage
+            .getElement(mainPage.missControlBottomTitle)
             .should("be.visible")
             .should("include.text", "Mission Control");
-        mainPage.socialBottomTitle
+        mainPage
+            .getElement(mainPage.socialBottomTitle)
             .should("be.visible")
             .should("include.text", "Social");
     });
     it("(Check element is visible)", () => {
-        mainPage.selectElement(mainPage.hiddenElement).should("not.exist");
+        mainPage.getElement(mainPage.hiddenElement).should("not.exist");
         cy.scrollTo("center");
         cy.wait(2000);
-        mainPage.selectElement(mainPage.hiddenElement).should("be.visible");
+        mainPage.getElement(mainPage.hiddenElement).should("be.visible");
     });
 });
